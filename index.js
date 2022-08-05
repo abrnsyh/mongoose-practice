a5083913919dd80d31d9787db19d3c2e1c8876d5
abrnsyh <abriansyah1099@gmail.com>
2022-08-05 09:39:14 +0700

    add error index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const router = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-dbConnection.then(() => console.log("Berhasil Connect")).catch((error) => console.log("error"));
+dbConnection.then(() => console.log("Berhasil Connect")).catch((error) => console.log("error", error));
 
 app.use(cors());
 app.use(express.json());
